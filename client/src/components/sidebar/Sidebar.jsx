2fd97f51bb2961437a349d6d7faf6eeac2aef1d0
Manikanta528 <chmanikanta528@gmail.com>
2022-05-21 14:11:25 +0530

    "update about us"

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -18,7 +18,7 @@ export default function Sidebar() {
         <div className="sidebarItem">
           <span className="sidebarTitle">About Us</span>
           <img src="https://images.unsplash.com/photo-1471107340929-a87cd0f5b5f3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1373&q=80" alt="about me" />
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+          <p style={{ textAlign:"center"}}>Online Blog System is a service used to manage and publish content on the internet.  </p>
         </div>
         <div className="sidebarItem">
         <span className="sidebarTitle">Categories</span>
